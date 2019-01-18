c95ec15a60c183b786dcbdb4a33974d92ffc0b56
Kersten Schuster <schuster@cs.rwth-aachen.de>
2019-01-18 13:40:50 +0100

    fixed aabb computation

--- src/tg/detail/functions/aabb.hh
+++ src/tg/detail/functions/aabb.hh
@@ -12,29 +12,26 @@ box<D, ScalarT> aabb(pos<D, ScalarT> const& v)
 }
 
 template <int D, class ScalarT>
-box<D, ScalarT> aabb(triangle<D, ScalarT> const& v)
-{
-    return aabb(v.v0, v.v1, v.v2);
-}
-
-template <int D, class ScalarT>
 box<D, ScalarT> aabb(box<D, ScalarT> const& b)
 {
     return b;
 }
 
-template <int D, class ScalarT, class PrimT, class... PrimsT>
-box<D, ScalarT> aabb(box<D, ScalarT> const& b, PrimT const& prim, PrimsT const&... prims)
+template <int D, class ScalarT>
+box<D, ScalarT> aabb(triangle<D, ScalarT> const& t)
 {
-    auto bp = aabb(prim);
-    auto b2 = {min(b.min, bp.min), max(b.max, bp.max)};
-    return aabb(b2, prims...);
+    return aabb(t.v0, t.v1, t.v2);
 }
 
-template <class PrimT, class... PrimsT>
-auto aabb(PrimT const& prim, PrimsT const&... prims) -> decltype(aabb(prim))
+template <class PrimA, class PrimB, class... PrimsT>
+auto aabb(PrimA const& pa, PrimB const& pb, PrimsT const&... prims) -> decltype(aabb(pa))
 {
-    return aabb(aabb(prim), prims...);
+    auto ba = aabb(pa);
+    auto bb = aabb(pb);
+    static_assert(std::is_same<decltype(ba), decltype(bb)>::value, "all arguments must have the same aabb box type");
+    auto b = decltype(ba)(min(ba.min, bb.min), max(ba.max, bb.max));
+    return aabb(b, prims...);
 }
+
 
 } // namespace tg
