2b1e2618bdfcddabc7c2f970b238e62fe7a64afb
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-01-21 08:40:51 +0100

    made aabb constexpr

--- src/tg/detail/functions/aabb.hh
+++ src/tg/detail/functions/aabb.hh
@@ -6,25 +6,25 @@
 namespace tg
 {
 template <int D, class ScalarT>
-box<D, ScalarT> aabb(pos<D, ScalarT> const& v)
+constexpr box<D, ScalarT> aabb(pos<D, ScalarT> const& v)
 {
     return {v, v};
 }
 
 template <int D, class ScalarT>
-box<D, ScalarT> aabb(box<D, ScalarT> const& b)
+constexpr box<D, ScalarT> aabb(box<D, ScalarT> const& b)
 {
     return b;
 }
 
 template <int D, class ScalarT>
-box<D, ScalarT> aabb(triangle<D, ScalarT> const& t)
+constexpr box<D, ScalarT> aabb(triangle<D, ScalarT> const& t)
 {
     return aabb(t.v0, t.v1, t.v2);
 }
 
 template <class PrimA, class PrimB, class... PrimsT>
-auto aabb(PrimA const& pa, PrimB const& pb, PrimsT const&... prims) -> decltype(aabb(pa))
+constexpr auto aabb(PrimA const& pa, PrimB const& pb, PrimsT const&... prims) -> decltype(aabb(pa))
 {
     auto ba = aabb(pa);
     auto bb = aabb(pb);
