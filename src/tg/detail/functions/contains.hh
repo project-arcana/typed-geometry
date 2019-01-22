e7d676b1448469d998d700fad5a25b47f1066c28
Kersten Schuster <schuster@cs.rwth-aachen.de>
2019-01-22 13:43:31 +0100

    allow specifying an epsilon for contains functions

--- src/tg/detail/functions/contains.hh
+++ src/tg/detail/functions/contains.hh
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include "../../types/box.hh"
 #include "../../types/pos.hh"
 #include "../../types/triangle.hh"
@@ -14,46 +15,49 @@
 namespace tg
 {
 template <int D, class ScalarT>
-constexpr bool contains(pos<D, ScalarT> const& b, pos<D, ScalarT> const& o)
+constexpr bool contains(pos<D, ScalarT> const& b, pos<D, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
+    if (eps > 0)
+        return distance2(b, o) < eps * eps;
     return b == o;
 }
 
 template <class ScalarT>
-constexpr bool contains(box<1, ScalarT> const& b, pos<1, ScalarT> const& o)
+constexpr bool contains(box<1, ScalarT> const& b, pos<1, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
-    return b.min.x <= o.x && o.x <= b.max.x;
+    return b.min.x - eps <= o.x && o.x <= b.max.x + eps;
 }
 template <class ScalarT>
-constexpr bool contains(box<2, ScalarT> const& b, pos<2, ScalarT> const& o)
+constexpr bool contains(box<2, ScalarT> const& b, pos<2, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
-    return b.min.x <= o.x && o.x <= b.max.x && //
-           b.min.y <= o.y && o.y <= b.max.y;
+    return b.min.x - eps <= o.x && o.x <= b.max.x + eps && //
+           b.min.y - eps <= o.y && o.y <= b.max.y + eps;
 }
 template <class ScalarT>
-constexpr bool contains(box<3, ScalarT> const& b, pos<3, ScalarT> const& o)
+constexpr bool contains(box<3, ScalarT> const& b, pos<3, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
-    return b.min.x <= o.x && o.x <= b.max.x && //
-           b.min.y <= o.y && o.y <= b.max.y && //
-           b.min.z <= o.z && o.z <= b.max.z;
+    return b.min.x - eps <= o.x && o.x <= b.max.x + eps && //
+           b.min.y - eps <= o.y && o.y <= b.max.y + eps && //
+           b.min.z - eps <= o.z && o.z <= b.max.z + eps;
 }
 template <class ScalarT>
-constexpr bool contains(box<4, ScalarT> const& b, pos<4, ScalarT> const& o)
+constexpr bool contains(box<4, ScalarT> const& b, pos<4, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
-    return b.min.x <= o.x && o.x <= b.max.x && //
-           b.min.y <= o.y && o.y <= b.max.y && //
-           b.min.z <= o.z && o.z <= b.max.z && //
-           b.min.w <= o.w && o.w <= b.max.w;
+    return b.min.x - eps <= o.x && o.x <= b.max.x + eps && //
+           b.min.y - eps <= o.y && o.y <= b.max.y + eps && //
+           b.min.z - eps <= o.z && o.z <= b.max.z + eps && //
+           b.min.w - eps <= o.w && o.w <= b.max.w + eps;
 }
 
 template <int D, class ScalarT>
-constexpr bool contains(box<D, ScalarT> const& b, box<D, ScalarT> const& o)
+constexpr bool contains(box<D, ScalarT> const& b, box<D, ScalarT> const& o, ScalarT eps = ScalarT(0))
 {
-    return contains(b, o.min) && contains(b, o.max);
+    return contains(b, o.min, eps) && contains(b, o.max, eps);
 }
 
+// Note that eps is used to compare 2D areas, not 1D lengths
 template <class ScalarT>
-constexpr bool contains(triangle<2, ScalarT> const& t, pos<2, ScalarT> const& p)
+constexpr bool contains(triangle<2, ScalarT> const& t, pos<2, ScalarT> const& p, ScalarT eps = ScalarT(0))
 {
     auto pv0 = t.v0 - p;
     auto pv1 = t.v1 - p;
@@ -62,7 +66,11 @@ constexpr bool contains(triangle<2, ScalarT> const& t, pos<2, ScalarT> const& p)
     auto A1 = cross(pv2, pv0);
     auto A2 = cross(pv0, pv1);
 
-    return ((A0 >= 0) == (A1 >= 0)) && ((A1 >= 0) == (A2 >= 0));
+    if (eps > 0)
+        return (A0 >= -std::copysign(eps, A0) == (A1 >= -std::copysign(eps, A0))) && //
+               (A1 >= -std::copysign(eps, A0) == (A2 >= -std::copysign(eps, A0)));
+
+    return ((A0 >= -eps) == (A1 >= -eps)) && ((A1 >= -eps) == (A2 >= -eps));
 }
 
 } // namespace tg
