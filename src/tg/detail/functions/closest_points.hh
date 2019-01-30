b815317f657153e068d591095f60dedfef74100e
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-01-30 05:48:20 +0100

    default pt impl for closest points

--- src/tg/detail/functions/closest_points.hh
+++ src/tg/detail/functions/closest_points.hh
@@ -19,21 +19,16 @@ constexpr pair<pos<D, ScalarT>, pos<D, ScalarT>> closest_points(pos<D, ScalarT> 
     return {a, b};
 }
 
-template <int D, class ScalarT>
-constexpr pair<pos<D, ScalarT>, pos<D, ScalarT>> closest_points(line<D, ScalarT> const& l, pos<D, ScalarT> const& p)
+// Default implementation for points:
+template <int D, class ScalarT, class ObjectT>
+constexpr auto closest_points(ObjectT const& o, pos<D, ScalarT> const& p) -> pair<decltype(project(p, o)), pos<D, ScalarT>>
 {
-    return {project(p, l), p};
+    return {project(p, o), p};
+}
+template <int D, class ScalarT, class ObjectT>
+constexpr auto closest_points(pos<D, ScalarT> const& p, ObjectT const& o) -> pair<pos<D, ScalarT>, decltype(project(p, o))>
+{
+    return {p, project(p, o)};
 }
 
-template <int D, class ScalarT>
-constexpr pair<pos<D, ScalarT>, pos<D, ScalarT>> closest_points(segment<D, ScalarT> const& s, pos<D, ScalarT> const& p)
-{
-    return {project(p, s), p};
-}
-
-template <class ScalarT>
-constexpr pair<pos<3, ScalarT>, pos<3, ScalarT>> closest_points(plane const& pl, pos<3, ScalarT> const& p)
-{
-    return {project(p, pl), p};
-}
 } // namespace tg
