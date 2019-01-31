641334ec7f4f60e0f07c7836b2f70e02760829f8
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-01-31 07:29:55 +0100

    determinants up to 3x3, submatrix/subvector

---
+++ src/tg/detail/constexpr_helper.hh
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "../types/vec.hh"
+
+namespace tg
+{
+namespace detail
+{
+template <class ScalarT>
+constexpr ScalarT& csubscript(vec<1, ScalarT>& v, int i)
+{
+    return v.x;
+}
+template <class ScalarT>
+constexpr ScalarT const& csubscript(vec<1, ScalarT> const& v, int i)
+{
+    return v.x;
+}
+template <class ScalarT>
+constexpr ScalarT const& csubscript(vec<2, ScalarT> const& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else
+        return v.y;
+}
+template <class ScalarT>
+constexpr ScalarT& csubscript(vec<2, ScalarT>& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else
+        return v.y;
+}
+template <class ScalarT>
+constexpr ScalarT const& csubscript(vec<3, ScalarT> const& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else if (i == 1)
+        return v.y;
+    else
+        return v.z;
+}
+template <class ScalarT>
+constexpr ScalarT& csubscript(vec<3, ScalarT>& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else if (i == 1)
+        return v.y;
+    else
+        return v.z;
+}
+template <class ScalarT>
+constexpr ScalarT& csubscript(vec<4, ScalarT>& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else if (i == 1)
+        return v.y;
+    else if (i == 2)
+        return v.z;
+    else
+        return v.w;
+}
+template <class ScalarT>
+constexpr ScalarT const& csubscript(vec<4, ScalarT> const& v, int i)
+{
+    if (i == 0)
+        return v.x;
+    else if (i == 1)
+        return v.y;
+    else if (i == 2)
+        return v.z;
+    else
+        return v.w;
+}
+} // namespace detail
+} // namespace tg
---
+++ src/tg/detail/functions/determinant.hh
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "../../types/mat.hh"
+#include "submatrix.hh"
+
+namespace tg
+{
+template <class ScalarT>
+constexpr ScalarT determinant(mat<1, 1, ScalarT> const& a)
+{
+    return a[0].x;
+}
+template <class ScalarT>
+constexpr ScalarT determinant(mat<2, 2, ScalarT> const& a)
+{
+    return a[0].x * a[1].y - a[0].y * a[1].x;
+}
+template <class ScalarT>
+constexpr ScalarT determinant(mat<3, 3, ScalarT> const& a)
+{
+    return a[0].x * (a[1].y * a[2].z - a[2].y * a[1].z) + //
+           a[1].x * (a[2].y * a[0].z - a[0].y * a[2].z) + //
+           a[2].x * (a[0].y * a[1].z - a[0].z * a[1].y);
+}
+// template <class ScalarT>
+// constexpr ScalarT determinant(mat<4, 4, ScalarT> const& a)
+// {
+//     // TODO
+// }
+} // namespace tg
