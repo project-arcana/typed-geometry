26b3f5c972c9f43383b2a6b4c0cf34250a3c018a
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-02-01 18:10:54 +0100

    added constexpr to inverse

--- src/tg/detail/functions/inverse.hh
+++ src/tg/detail/functions/inverse.hh
@@ -8,12 +8,12 @@
 namespace tg
 {
 template <class ScalarT>
-mat<1, 1, fractional_result<ScalarT>> inverse(mat<1, 1, ScalarT> const& m)
+constexpr mat<1, 1, fractional_result<ScalarT>> inverse(mat<1, 1, ScalarT> const& m)
 {
     return {ScalarT(1.0) / m[0].x};
 }
 template <class ScalarT>
-mat<2, 2, fractional_result<ScalarT>> inverse(mat<2, 2, ScalarT> const& m)
+constexpr mat<2, 2, fractional_result<ScalarT>> inverse(mat<2, 2, ScalarT> const& m)
 {
     auto invdet = ScalarT(1.0) / determinant(m);
 
@@ -25,7 +25,7 @@ mat<2, 2, fractional_result<ScalarT>> inverse(mat<2, 2, ScalarT> const& m)
     return res;
 }
 template <class ScalarT>
-mat<3, 3, fractional_result<ScalarT>> inverse(mat<3, 3, ScalarT> const& m)
+constexpr mat<3, 3, fractional_result<ScalarT>> inverse(mat<3, 3, ScalarT> const& m)
 {
     auto invdet = ScalarT(1.0) / determinant(m);
 
@@ -42,7 +42,7 @@ mat<3, 3, fractional_result<ScalarT>> inverse(mat<3, 3, ScalarT> const& m)
     return res;
 }
 // template <class ScalarT>
-// mat<4, 4, fractional_result<ScalarT>> inverse(mat<4, 4, ScalarT> const& m)
+// constexpr mat<4, 4, fractional_result<ScalarT>> inverse(mat<4, 4, ScalarT> const& m)
 // {
 //     auto invdet = ScalarT(1.0) / determinant(m);
 //
