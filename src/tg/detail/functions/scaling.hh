fc14d63afe6d73aca0c78f3698e9532abd8e4943
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-03-16 10:07:56 +0100

    restored c++14 compat

--- src/tg/detail/functions/scaling.hh
+++ src/tg/detail/functions/scaling.hh
@@ -8,6 +8,6 @@ namespace tg
 template <int D, class T>
 constexpr mat<D + 1, D + 1, T> scaling(size<D, T> const& v)
 {
-    return mat<D + 1, D + 1, T>::diag(vec<D + 1, T>(vec(v), T(1)));
+    return mat<D + 1, D + 1, T>::diag(vec<D + 1, T>(vec<D, T>(v), T(1)));
 }
 } // namespace tg
--- src/tg/detail/functions/translation.hh
+++ src/tg/detail/functions/translation.hh
@@ -16,7 +16,7 @@ template <int D, class T>
 constexpr mat<D + 1, D + 1, T> translation(pos<D, T> const& v)
 {
     auto m = mat<D + 1, D + 1, T>::identity;
-    m[D] = vec<D + 1, T>(vec(v), T(1));
+    m[D] = vec<D + 1, T>(vec<D, T>(v), T(1));
     return m;
 }
 } // namespace tg
