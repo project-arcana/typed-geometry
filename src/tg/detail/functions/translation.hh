b9d04da2ab66ba8b8800c9cf4cdb3460a89497a0
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-04-01 13:09:30 +0200

    disabled pos-translation for now

--- src/tg/detail/functions/translation.hh
+++ src/tg/detail/functions/translation.hh
@@ -12,11 +12,13 @@ constexpr mat<D + 1, D + 1, T> translation(vec<D, T> const& v)
     m[D] = vec<D + 1, T>(v, T(1));
     return m;
 }
-template <int D, class T>
-constexpr mat<D + 1, D + 1, T> translation(pos<D, T> const& v)
-{
-    auto m = mat<D + 1, D + 1, T>::identity;
-    m[D] = vec<D + 1, T>(vec<D, T>(v), T(1));
-    return m;
-}
+
+// Note: revisit once we have use cases
+// template <int D, class T>
+// constexpr mat<D + 1, D + 1, T> translation(pos<D, T> const& v)
+// {
+//     auto m = mat<D + 1, D + 1, T>::identity;
+//     m[D] = vec<D + 1, T>(vec<D, T>(v), T(1));
+//     return m;
+// }
 } // namespace tg
