b4d3d88c7ded7003dc114843ea940ecc0aefd6b5
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-04-11 10:41:01 +0200

    added built-in epsilon

--- src/tg/detail/constants.hh
+++ src/tg/detail/constants.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../types/angle.hh"
+#include "errors.hh"
 
 namespace tg
 {
@@ -12,6 +13,13 @@ constexpr angle<T> pi = angle<T>::from_radians(static_cast<T>(3.1415926535897932
 
 template <class T>
 constexpr T nan = static_cast<T>(0.0 / 0.0);
+
+template <class T>
+constexpr T epsilon = type_error::unsupported_type<T>::value;
+template <>
+constexpr float epsilon<float> = 1.19209290E-07F; // FLT_EPSILON
+template <>
+constexpr double epsilon<double> = 2.2204460492503131e-16; // DBL_EPSILON
 
 // TODO: proper min vs. lowest
 // template <class T>
