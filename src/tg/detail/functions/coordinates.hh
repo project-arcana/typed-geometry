5ecb056117563befcbfe14d58d360ffdc4e0d332
Kersten Schuster <schuster@cs.rwth-aachen.de>
2019-04-12 11:50:58 +0200

    barycentric coordinates for 3d triangle.

--- src/tg/detail/functions/coordinates.hh
+++ src/tg/detail/functions/coordinates.hh
@@ -33,6 +33,23 @@ constexpr array<ScalarT, 3> coordinates(triangle<2, ScalarT> const& t, pos<2, Sc
     return {{A0 * A_inv, A1 * A_inv, A2 * A_inv}};
 }
 
+template <class ScalarT>
+constexpr array<ScalarT, 3> coordinates(triangle<3, ScalarT> const& t, pos<3, ScalarT> const& p)
+{
+    auto pv0 = t.v0 - p;
+    auto pv1 = t.v1 - p;
+    auto pv2 = t.v2 - p;
+    auto n = cross(t.v1 - t.v0, t.v2 - t.v0);
+    auto A = length(n);
+    n /= A;
+    auto A0 = dot(cross(pv1, pv2), n);
+    auto A1 = dot(cross(pv2, pv0), n);
+    auto A2 = dot(cross(pv0, pv1), n);
+
+    auto A_inv = ScalarT(1) / A;
+    return {{A0 * A_inv, A1 * A_inv, A2 * A_inv}};
+}
+
 template <int D, class ScalarT>
 constexpr ScalarT coordinates(segment<D, ScalarT> const& s, pos<D, ScalarT> const& p)
 {
