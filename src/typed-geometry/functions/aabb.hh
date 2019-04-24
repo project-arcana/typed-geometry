8f9c4eeb0fa79a3a7bc464d0787dd5a89c70ad14
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-04-24 19:26:22 +0200

    renamed aabb to aabb_of

--- src/typed-geometry/functions/aabb.hh
+++ src/typed-geometry/functions/aabb.hh
@@ -6,31 +6,31 @@
 namespace tg
 {
 template <int D, class ScalarT>
-TG_NODISCARD constexpr box<D, ScalarT> aabb(pos<D, ScalarT> const& v)
+TG_NODISCARD constexpr box<D, ScalarT> aabb_of(pos<D, ScalarT> const& v)
 {
     return {v, v};
 }
 
 template <int D, class ScalarT>
-TG_NODISCARD constexpr box<D, ScalarT> aabb(box<D, ScalarT> const& b)
+TG_NODISCARD constexpr box<D, ScalarT> aabb_of(box<D, ScalarT> const& b)
 {
     return b;
 }
 
 template <int D, class ScalarT>
-TG_NODISCARD constexpr box<D, ScalarT> aabb(triangle<D, ScalarT> const& t)
+TG_NODISCARD constexpr box<D, ScalarT> aabb_of(triangle<D, ScalarT> const& t)
 {
-    return aabb(t.pos0, t.pos1, t.pos2);
+    return aabb_of(t.pos0, t.pos1, t.pos2);
 }
 
 template <class PrimA, class PrimB, class... PrimsT>
-TG_NODISCARD constexpr auto aabb(PrimA const& pa, PrimB const& pb, PrimsT const&... prims) -> decltype(aabb(pa))
+TG_NODISCARD constexpr auto aabb_of(PrimA const& pa, PrimB const& pb, PrimsT const&... prims) -> decltype(aabb_of(pa))
 {
-    auto ba = aabb(pa);
-    auto bb = aabb(pb);
+    auto ba = aabb_of(pa);
+    auto bb = aabb_of(pb);
     static_assert(is_same<decltype(ba), decltype(bb)>, "all arguments must have the same aabb box type");
     auto b = decltype(ba)(min(ba.min, bb.min), max(ba.max, bb.max));
-    return aabb(b, prims...);
+    return aabb_of(b, prims...);
 }
 
 
