7fa9ce6c6e7c38b85acf849a8749d39e1a2db201
Julian Schakib <julian.schakib@rwth-aachen.de>
2019-04-24 19:33:48 +0200

    implemented orthogonal of dir2, dir3, vec2, vec3.

--- src/typed-geometry/functions/orthogonal.hh
+++ src/typed-geometry/functions/orthogonal.hh
@@ -4,13 +4,45 @@
 #include <typed-geometry/types/dir.hh>
 #include <typed-geometry/types/vec.hh>
 
-// Computes orthogonal vector or direction respectively
+//#include <typed-geometry/detail/operators/ops_vec.hh>
+
+#include <typed-geometry/common/scalar_math.hh>
+
+// Computes orthogonal direction
 namespace tg
 {
-// Base case for vector
-template <int D, class ScalarT>
-TG_NODISCARD constexpr vec<D, ScalarT> distance2(vec<D, ScalarT> const& v)
+template <class ScalarT>
+TG_NODISCARD constexpr dir<2, ScalarT> orthogonal(vec<2, ScalarT> const& v)
 {
-    // TODO find strongest component, use that unit vector in cross
+    // TODO check if input is 0 and reject/warn?
+    // TODO alternative: x' = -y, y' = x ?
+    if(abs(v.x) > abs(v.y))
+        return normalize(cross(v, vec2(0.0f, 1.0f)));
+    else
+        return normalize(cross(v, vec2(1.0f, 0.0f)));
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr dir<3, ScalarT> orthogonal(vec<3, ScalarT> const& v)
+{
+    auto b = vec3::zero;
+    if(abs(v.x) > abs(v.y) && abs(v.x) > abs(v.z))
+        b[2] = 1.0f;
+    else if(abs(v.y) > abs(v.x) && abs(v.y) > abs(v.z))
+        b[0] = 1.0f;
+    else
+        b[1] = 1.0f;
+    return normalize(cross(v, b));
+}
+template <class ScalarT>
+TG_NODISCARD constexpr dir<2, ScalarT> orthogonal(dir<2, ScalarT> const& d)
+{
+    return normalize(orthogonal(vec<2, ScalarT>(d)));
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr dir<3, ScalarT> orthogonal(dir<3, ScalarT> const& d)
+{
+    return normalize(orthogonal(vec<3, ScalarT>(d)));
 }
 } // namespace tg
