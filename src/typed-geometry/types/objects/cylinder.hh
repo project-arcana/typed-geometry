2539a5a794492ad29a1a1e7d8fd6b3294cceadf3
Julian Schakib <julian.schakib@rwth-aachen.de>
2019-05-04 20:27:06 +0200

    descriptions of objects, is tube a hollow cylinder?

--- src/typed-geometry/types/objects/cylinder.hh
+++ src/typed-geometry/types/objects/cylinder.hh
@@ -35,10 +35,11 @@ struct cylinder<3, ScalarT>
     disk_t base;
 
     constexpr cylinder() = default;
-    constexpr cylinder(pos_t bc, pos_t tc, ScalarT r)
+    /* //TODO this would require length and normalize, do we want that?
+      constexpr cylinder(pos_t bc, pos_t tc, ScalarT r)
       : height(length(tc - bc)), base(disk_t(bc, r, normalize(tc - bc))), center(bc + height * base.normal)
     {
-    }
+    }*/
     constexpr cylinder(pos_t c, ScalarT r, ScalarT h, dir_t n = dir_t::pos_y) : center(c), height(h), base(disk_t(c - h / 2 * n, r, n)) {}
 };
 } // namespace tg
