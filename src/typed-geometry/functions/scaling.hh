0c65f3ae381cf53588587ae1e570f13f961d1a9d
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-07-04 10:53:22 +0200

    added convenience overloads for scaling

--- src/typed-geometry/functions/scaling.hh
+++ src/typed-geometry/functions/scaling.hh
@@ -10,4 +10,20 @@ TG_NODISCARD constexpr mat<D + 1, D + 1, T> scaling(size<D, T> const& v)
 {
     return mat<D + 1, D + 1, T>::diag(vec<D + 1, T>(vec<D, T>(v), T(1)));
 }
+template <int D, class T>
+TG_NODISCARD constexpr mat<D + 1, D + 1, T> scaling(T v)
+{
+    return mat<D + 1, D + 1, T>::diag(vec<D + 1, T>(vec<D, T>(v), T(1)));
+}
+
+template <class T>
+TG_NODISCARD constexpr mat<3, 3, T> scaling(T sx, T sy)
+{
+    return mat<3, 3, T>::diag(vec<3, T>(sx, sy, T(1)));
+}
+template <class T>
+TG_NODISCARD constexpr mat<4, 4, T> scaling(T sx, T sy, T sz)
+{
+    return mat<4, 4, T>::diag(vec<4, T>(sx, sy, sz, T(1)));
+}
 } // namespace tg
