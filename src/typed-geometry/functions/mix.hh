02658d64012f9ba673b0844299af3ceeac09ce56
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-08-09 15:32:10 +0200

    improved mix

--- src/typed-geometry/functions/mix.hh
+++ src/typed-geometry/functions/mix.hh
@@ -15,24 +15,24 @@ TG_NODISCARD constexpr ScalarT mix(ScalarT v0, ScalarT v1, ScalarT t)
     return v0 + t * (v1 - v0);
 }
 template <class ScalarT, class = enable_if<is_floating_point<ScalarT>>>
-TG_NODISCARD constexpr angle_t<ScalarT> mix(angle_t<ScalarT> v0, angle_t<ScalarT> v1, ScalarT t)
+TG_NODISCARD constexpr angle_t<ScalarT> mix(angle_t<ScalarT> v0, angle_t<ScalarT> v1, tg::dont_deduce<ScalarT> t)
 {
     return tg::radians(tg::mix(v0.radians(), v1.radians(), t));
 }
 
 template <int D, class ScalarT, class = enable_if<is_floating_point<ScalarT>>>
-TG_NODISCARD constexpr vec<D, ScalarT> mix(vec<D, ScalarT> const& v0, vec<D, ScalarT> const& v1, ScalarT t)
+TG_NODISCARD constexpr vec<D, ScalarT> mix(vec<D, ScalarT> const& v0, vec<D, ScalarT> const& v1, tg::dont_deduce<ScalarT> t)
 {
     return v0 + t * (v1 - v0);
 }
 template <int D, class ScalarT, class = enable_if<is_floating_point<ScalarT>>>
-TG_NODISCARD constexpr pos<D, ScalarT> mix(pos<D, ScalarT> const& v0, pos<D, ScalarT> const& v1, ScalarT t)
+TG_NODISCARD constexpr pos<D, ScalarT> mix(pos<D, ScalarT> const& v0, pos<D, ScalarT> const& v1, tg::dont_deduce<ScalarT> t)
 {
     return v0 + t * (v1 - v0);
 }
 
 template <class ScalarT, class = enable_if<is_floating_point<ScalarT>>>
-TG_NODISCARD constexpr color<3, ScalarT> mix(color<3, ScalarT> const& c0, color<3, ScalarT> const& c1, ScalarT t)
+TG_NODISCARD constexpr color<3, ScalarT> mix(color<3, ScalarT> const& c0, color<3, ScalarT> const& c1, tg::dont_deduce<ScalarT> t)
 {
     return {
         mix(c0.r, c1.r, t), //
@@ -41,7 +41,7 @@ TG_NODISCARD constexpr color<3, ScalarT> mix(color<3, ScalarT> const& c0, color<
     };
 }
 template <class ScalarT, class = enable_if<is_floating_point<ScalarT>>>
-TG_NODISCARD constexpr color<4, ScalarT> mix(color<4, ScalarT> const& c0, color<4, ScalarT> const& c1, ScalarT t)
+TG_NODISCARD constexpr color<4, ScalarT> mix(color<4, ScalarT> const& c0, color<4, ScalarT> const& c1, tg::dont_deduce<ScalarT> t)
 {
     return {
         mix(c0.r, c1.r, t), //
