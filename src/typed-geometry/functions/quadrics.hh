28515714ae2341849c68f4582e6fcd46bf747ed1
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-09-24 17:04:16 +0200

    left scalar mat ops, more quadrics

--- src/typed-geometry/functions/quadrics.hh
+++ src/typed-geometry/functions/quadrics.hh
@@ -30,12 +30,12 @@ TG_NODISCARD constexpr quadric<D, ScalarT> plane_quadric(pos<D, ScalarT> const& 
 template <int D, class ScalarT>
 TG_NODISCARD constexpr quadric<D, ScalarT> probabilistic_plane_quadric(pos<D, ScalarT> const& mean_p,
                                                                        vec<D, ScalarT> const& mean_n,
-                                                                       dont_deduce<ScalarT> sigma_p,
-                                                                       dont_deduce<ScalarT> sigma_n)
+                                                                       dont_deduce<ScalarT> stddev_p,
+                                                                       dont_deduce<ScalarT> stddev_n)
 {
     auto const p = vec(mean_p);
-    auto const sn2 = sigma_n * sigma_n;
-    auto const sp2 = sigma_p * sigma_p;
+    auto const sn2 = stddev_n * stddev_n;
+    auto const sp2 = stddev_p * stddev_p;
     auto const d = dot(mean_p, mean_n);
 
     auto A = self_outer_product(mean_n);
@@ -76,5 +76,80 @@ TG_NODISCARD constexpr quadric<D, ScalarT> triangle_quadric(pos<D, ScalarT> cons
     auto const det = dot(pxq, r);
 
     return quadric<D, ScalarT>::from_coefficients(self_outer_product(xsum), xsum * det, det * det);
+}
+
+template <int D, class ScalarT>
+TG_NODISCARD constexpr quadric<D, ScalarT> probabilistic_triangle_quadric(pos<D, ScalarT> const& mean_p,
+                                                                          pos<D, ScalarT> const& mean_q,
+                                                                          pos<D, ScalarT> const& mean_r,
+                                                                          dont_deduce<ScalarT> stddev)
+{
+    auto const cross_product_squared_transpose = [](vec<3, ScalarT> const& v) {
+        auto const a = v.x;
+        auto const b = v.y;
+        auto const c = v.z;
+        auto const a2 = a * a;
+        auto const b2 = b * b;
+        auto const c2 = c * c;
+
+        mat<3, 3, ScalarT> M;
+
+        M[0][0] = b2 + c2;
+        M[1][1] = a2 + c2;
+        M[2][2] = a2 + b2;
+
+        M[1][0] = M[0][1] = -a * b;
+        M[2][0] = M[0][2] = -a * c;
+        M[2][1] = M[1][2] = -b * c;
+
+        return M;
+    };
+
+    auto const sigma = stddev * stddev;
+    auto const p = tg::vec(mean_p);
+    auto const q = tg::vec(mean_q);
+    auto const r = tg::vec(mean_r);
+
+    auto const pxq = cross(p, q);
+    auto const qxr = cross(q, r);
+    auto const rxp = cross(r, p);
+
+    auto const det_pqr = dot(pxq, r);
+
+    auto const cross_pqr = pxq + qxr + rxp;
+
+    auto const pmq = p - q;
+    auto const qmr = q - r;
+    auto const rmp = r - p;
+
+    mat<3, 3, ScalarT> A;
+
+    A += self_outer_product(cross_pqr);
+
+    A += (cross_product_squared_transpose(pmq) + //
+          cross_product_squared_transpose(qmr) + //
+          cross_product_squared_transpose(rmp))
+         * sigma;
+
+    auto ss = sigma * sigma;
+    auto ss6 = 6 * ss;
+    auto ss2 = 2 * ss;
+    A[0][0] += ss6;
+    A[1][1] += ss6;
+    A[2][2] += ss6;
+
+    tg::vec<3, ScalarT> b;
+
+    b += det_pqr * cross_pqr;
+
+    b -= sigma * (cross(pmq, pxq) + cross(qmr, qxr) + cross(rmp, rxp));
+
+    b += ss2 * (p + q + r);
+
+    ScalarT c = det_pqr * det_pqr;
+
+    // TODO: more c
+
+    return quadric<D, ScalarT>::from_coefficients(A, b, c);
 }
 }
