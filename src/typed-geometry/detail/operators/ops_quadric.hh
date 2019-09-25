1599c52b4f3b8b013a270054bfe8f343842d54b4
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-09-25 06:42:08 +0200

    quadric ops

--- src/typed-geometry/detail/operators/ops_quadric.hh
+++ src/typed-geometry/detail/operators/ops_quadric.hh
@@ -12,5 +12,31 @@ TG_NODISCARD constexpr quadric<D, ScalarT> operator+(quadric<D, ScalarT> const& 
     return r;
 }
 
-// TODO: scalar scaling + division?
+template <class ScalarT>
+TG_NODISCARD constexpr quadric<3, ScalarT> operator*(quadric<3, ScalarT> q, dont_deduce<ScalarT> s)
+{
+    // q is already a copy
+    q.A00 *= s;
+    q.A01 *= s;
+    q.A02 *= s;
+    q.A11 *= s;
+    q.A12 *= s;
+    q.A22 *= s;
+    q.b0 *= s;
+    q.b1 *= s;
+    q.b2 *= s;
+    q.c *= s;
+    return q;
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr quadric<3, ScalarT> operator*(dont_deduce<ScalarT> s, quadric<3, ScalarT> q)
+{
+    return q * s;
+}
+template <class ScalarT>
+TG_NODISCARD constexpr quadric<3, ScalarT> operator/(quadric<3, ScalarT> q, dont_deduce<ScalarT> s)
+{
+    return q * (ScalarT(1) / s);
+}
 }
