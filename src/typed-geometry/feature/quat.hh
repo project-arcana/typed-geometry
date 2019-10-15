54b6e09a7c5b24d4d59e2bc8739ef6006d0a977e
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-10-15 05:38:57 +0200

    working on quats

--- src/typed-geometry/feature/quat.hh
+++ src/typed-geometry/feature/quat.hh
@@ -1,61 +1,109 @@
 #pragma once
 
 #include <typed-geometry/common/scalar_math.hh>
+#include <typed-geometry/functions/normalize.hh>
+#include <typed-geometry/types/dir.hh>
 #include <typed-geometry/types/quat.hh>
+#include <typed-geometry/types/mat.hh>
 #include <typed-geometry/types/vec.hh>
 
 namespace tg
 {
+// ================================= Name constructors =================================
+
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> quat<ScalarT>::from_axis_angle(dir<3, ScalarT> const& axis, angle_t<ScalarT> angle)
+{
+    auto a_half = angle * ScalarT(0.5);
+    auto [sa, ca] = sin_cos(a_half);
+    return {axis * sa, ca};
+}
+
+
 // ================================= Operators =================================
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator+(quat<ScalarT> const& a, quat<ScalarT> const& b)
+TG_NODISCARD constexpr quat<ScalarT> operator+(quat<ScalarT> const& q0, quat<ScalarT> const& q1)
 {
-    return {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+    return {q0.x + q1.x, q0.y + q1.y, q0.z + q1.z, q0.w + q1.w};
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator-(quat<ScalarT> const& a, quat<ScalarT> const& b)
+TG_NODISCARD constexpr quat<ScalarT> operator-(quat<ScalarT> const& q0, quat<ScalarT> const& q1)
 {
-    return {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
+    return {q0.x - q1.x, q0.y - q1.y, q0.z - q1.z, q0.w - q1.w};
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator*(quat<ScalarT> const& a, quat<ScalarT> const& b)
+TG_NODISCARD constexpr quat<ScalarT> operator*(quat<ScalarT> const& q0, quat<ScalarT> const& q1)
 {
     return {
-        a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y, //
-        a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x, //
-        a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w, //
-        a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w  //
+        q0.w * q1.x + q0.x * q1.w + q0.y * q1.z - q0.z * q1.y, //
+        q0.w * q1.y - q0.x * q1.z + q0.y * q1.w + q0.z * q1.x, //
+        q0.w * q1.z + q0.x * q1.y - q0.y * q1.x + q0.z * q1.w, //
+        q0.x * q1.x + q0.y * q1.y + q0.z * q1.z + q0.w * q1.w  //
     };
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator*(quat<ScalarT> const& a, dont_deduce<ScalarT> b)
+TG_NODISCARD constexpr quat<ScalarT> operator*(quat<ScalarT> const& q0, dont_deduce<ScalarT> q1)
 {
-    return {a.x * b, a.y * b, a.z * b, a.w * b};
+    return {q0.x * q1, q0.y * q1, q0.z * q1, q0.w * q1};
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator*(dont_deduce<ScalarT> a, quat<ScalarT> const& b)
+TG_NODISCARD constexpr quat<ScalarT> operator*(dont_deduce<ScalarT> q0, quat<ScalarT> const& q1)
 {
-    return {a * b.x, a * b.y, a * b.z, a * b.w};
+    return {q0 * q1.x, q0 * q1.y, q0 * q1.z, q0 * q1.w};
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator/(quat<ScalarT> const& a, dont_deduce<ScalarT> b)
+TG_NODISCARD constexpr quat<ScalarT> operator/(quat<ScalarT> const& q0, dont_deduce<ScalarT> q1)
 {
-    return a * (ScalarT(1) / b);
+    return q0 * (ScalarT(1) / q1);
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr quat<ScalarT> operator/(dont_deduce<ScalarT> a, quat<ScalarT> const& b)
+TG_NODISCARD constexpr quat<ScalarT> operator/(dont_deduce<ScalarT> q0, quat<ScalarT> const& q1)
 {
-    return {a / b.x, a / b.y, a / b.z, a / b.w};
+    return {q0 / q1.x, q0 / q1.y, q0 / q1.z, q0 / q1.w};
 }
 
 // ================================= Functions =================================
+
+
+template <class ScalarT>
+TG_NODISCARD constexpr ScalarT real(quat<ScalarT> const& q)
+{
+    return q.w;
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr vec<3, ScalarT> imaginary(quat<ScalarT> const& q)
+{
+    return {q.x, q.y, q.z};
+}
+
+// CAUTION: only works with normalized quats for now
+template <class ScalarT>
+TG_NODISCARD constexpr angle_t<ScalarT> angle_of(quat<ScalarT> const& q)
+{
+    return acos(q.w) * ScalarT(2);
+}
+
+// CAUTION: only works with normalized quats for now
+template <class ScalarT>
+TG_NODISCARD constexpr dir<3, ScalarT> axis_of(quat<ScalarT> const& q)
+{
+    return q.w * q.w >= ScalarT(1) ? dir<3, ScalarT>::pos_x : normalize(imaginary(q));
+}
+
+// CAUTION: only works with normalized quats for now
+template <class ScalarT>
+TG_NODISCARD constexpr angle_t<ScalarT> angle_between(quat<ScalarT> const& q0, quat<ScalarT> const& q1)
+{
+    return acos(saturate(dot(q0, q1))) * ScalarT(2);
+}
 
 template <class ScalarT>
 TG_NODISCARD constexpr ScalarT length_sqr(quat<ScalarT> const& q)
@@ -76,6 +124,18 @@ TG_NODISCARD constexpr quat<ScalarT> normalize(quat<ScalarT> const& q)
 }
 
 template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> conjugate(quat<ScalarT> const& q)
+{
+    return {-q.x, -q.y, -q.z, q.w};
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> inverse(quat<ScalarT> const& q)
+{
+    return conjugate(q) * (ScalarT(1) / length_sqr(q));
+}
+
+template <class ScalarT>
 TG_NODISCARD constexpr quat<ScalarT> normalize_safe(quat<ScalarT> const& q, dont_deduce<ScalarT> eps = ScalarT(0))
 {
     auto const l = length(q);
@@ -83,14 +143,71 @@ TG_NODISCARD constexpr quat<ScalarT> normalize_safe(quat<ScalarT> const& q, dont
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr ScalarT real(quat<ScalarT> const& q)
+TG_NODISCARD constexpr quat<ScalarT> dot(quat<ScalarT> const& q0, quat<ScalarT> const& q1)
 {
-    return q.w;
+    return {q0.x * q1.x, q0.y * q1.y, q0.z * q1.z, q0.w * q1.w};
 }
 
 template <class ScalarT>
-TG_NODISCARD constexpr vec<3, ScalarT> imaginary(quat<ScalarT> const& q)
+TG_NODISCARD constexpr quat<ScalarT> mix(quat<ScalarT> const& q0, quat<ScalarT> const& q1, dont_deduce<ScalarT> t)
 {
-    return {q.x, q.y, q.z};
+    return {mix(q0.x, q1.x, t), mix(q0.y, q1.y, t), mix(q0.z, q1.z, t), mix(q0.w, q1.w, t)};
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> lerp(quat<ScalarT> const& q0, quat<ScalarT> const& q1, dont_deduce<ScalarT> t)
+{
+    return mix(q0, q1, t);
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> nlerp(quat<ScalarT> const& q0, quat<ScalarT> const& q1, dont_deduce<ScalarT> t)
+{
+    return normalize(mix(q0, dot(q0, q1) >= ScalarT(0) ? q1 : -q1, t));
+}
+
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> nlerp_unaligned(quat<ScalarT> const& q0, quat<ScalarT> const& q1, dont_deduce<ScalarT> t)
+{
+    return normalize(mix(q0, q1, t));
+}
+
+// CAUTION: only works with normalized quats for now
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> slerp(quat<ScalarT> const& q0, quat<ScalarT> q1, dont_deduce<ScalarT> t)
+{
+    auto dotAB = dot(q0, q1);
+
+    // ensure alignment
+    if (dotAB < ScalarT(0))
+    {
+        q1 = -q1;
+        dotAB = -dotAB;
+    }
+
+    // for almost-same quaternions this prevents div-by-zero
+    if (dotAB >= ScalarT(1) - tg::epsilon<ScalarT>)
+        return lerp(q0, q1, t);
+
+    auto const alpha = acos(dotAB);
+    auto const alpha0 = (ScalarT(1) - t) * alpha;
+    auto const alpha1 = t * alpha;
+    return (q0 * sin(alpha0) + q1 * sin(alpha1)) / sin(alpha);
+}
+
+// CAUTION: only works with normalized quats for now
+template <class ScalarT>
+TG_NODISCARD constexpr quat<ScalarT> slerp_unaligned(quat<ScalarT> const& q0, quat<ScalarT> q1, dont_deduce<ScalarT> t)
+{
+    auto dotAB = dot(q0, q1);
+
+    // for almost-same quaternions this prevents div-by-zero
+    if (dotAB >= ScalarT(1) - tg::epsilon<ScalarT>)
+        return lerp(q0, q1, t);
+
+    auto const alpha = acos(dotAB);
+    auto const alpha0 = (ScalarT(1) - t) * alpha;
+    auto const alpha1 = t * alpha;
+    return (q0 * sin(alpha0) + q1 * sin(alpha1)) / sin(alpha);
 }
 }
--- src/typed-geometry/types/quat.hh
+++ src/typed-geometry/types/quat.hh
@@ -6,6 +6,7 @@
 #include "../detail/scalar_traits.hh"
 #include "../detail/utility.hh"
 #include "fwd.hh"
+#include "vec.hh"
 
 namespace tg
 {
@@ -28,8 +29,12 @@ struct quat
 
     TG_DECLARE_COMP_TYPE_4_NO_SWIZZLE(quat);
 
+    constexpr quat(vec<3, ScalarT> const& i, ScalarT r) : x(i.x), y(i.y), z(i.z), w(r) {}
+
     TG_NODISCARD constexpr bool operator==(quat const& rhs) const { return x == rhs.x && y == rhs.y && z == rhs.z && w == rhs.w; }
     TG_NODISCARD constexpr bool operator!=(quat const& rhs) const { return x != rhs.x || y != rhs.y || z != rhs.z || w != rhs.w; }
+
+    TG_NODISCARD static constexpr quat from_axis_angle(dir<3, ScalarT> const& axis, angle_t<ScalarT> angle);
 };
 template <class T>
 const quat<T> quat<T>::zero = {T(0), T(0), T(0), T(0)};
