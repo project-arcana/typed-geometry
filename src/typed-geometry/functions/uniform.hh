ed65ef55e8c67af6ac56f4a2293b410f7290bdcc
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-10-28 19:23:43 +0100

    fixed missing includes and long vs long long problem

--- src/typed-geometry/functions/uniform.hh
+++ src/typed-geometry/functions/uniform.hh
@@ -11,10 +11,14 @@
 #include <typed-geometry/types/objects/aabb.hh>
 #include <typed-geometry/types/objects/ball.hh>
 #include <typed-geometry/types/objects/box.hh>
+#include <typed-geometry/types/objects/capsule.hh>
 #include <typed-geometry/types/objects/circle.hh>
+#include <typed-geometry/types/objects/cylinder.hh>
 #include <typed-geometry/types/objects/disk.hh>
+#include <typed-geometry/types/objects/hemisphere.hh>
 #include <typed-geometry/types/objects/sphere.hh>
 #include <typed-geometry/types/objects/triangle.hh>
+#include <typed-geometry/types/objects/tube.hh>
 
 #include "math.hh"
 #include "minmax.hh"
@@ -76,14 +80,26 @@ template <class Rng>
     return r;
 }
 template <class Rng>
-[[nodiscard]] constexpr i64 uniform(Rng& rng, i64 a, i64 b_inc)
+[[nodiscard]] constexpr long uniform(Rng& rng, long a, long b_inc)
 {
-    i64 r = 0;
+    long r = 0;
     auto fa = f64(a);
     auto fb = f64(b_inc) + 1;
     do
     {
-        r = tg::ifloor(uniform(rng, fa, fb));
+        r = (long)tg::ifloor(uniform(rng, fa, fb));
+    } while (r > b_inc);
+    return r;
+}
+template <class Rng>
+[[nodiscard]] constexpr i64 uniform(Rng& rng, long long a, long long b_inc)
+{
+    long long r = 0;
+    auto fa = f64(a);
+    auto fb = f64(b_inc) + 1;
+    do
+    {
+        r = (long long)tg::ifloor(uniform(rng, fa, fb));
     } while (r > b_inc);
     return r;
 }
@@ -100,14 +116,26 @@ template <class Rng>
     return r;
 }
 template <class Rng>
-[[nodiscard]] constexpr u64 uniform(Rng& rng, u64 a, u64 b_inc)
+[[nodiscard]] constexpr unsigned long uniform(Rng& rng, unsigned long a, unsigned long b_inc)
 {
-    u64 r = 0;
+    unsigned long r = 0;
     auto fa = f64(a);
     auto fb = f64(b_inc) + 1;
     do
     {
-        r = u64(tg::ifloor(uniform(rng, fa, fb)));
+        r = (unsigned long)(tg::ifloor(uniform(rng, fa, fb)));
+    } while (r > b_inc);
+    return r;
+}
+template <class Rng>
+[[nodiscard]] constexpr unsigned long long uniform(Rng& rng, unsigned long long a, unsigned long long b_inc)
+{
+    unsigned long long r = 0;
+    auto fa = f64(a);
+    auto fb = f64(b_inc) + 1;
+    do
+    {
+        r = (unsigned long long)(tg::ifloor(uniform(rng, fa, fb)));
     } while (r > b_inc);
     return r;
 }
@@ -233,7 +261,7 @@ template <class ScalarT, class Rng>
     auto x = c.axis.pos1 - c.axis.pos0;
     auto h = length(x);
     auto sideArea = ScalarT(2) * c.radius * h; // * Pi, but that does not matter here
-    auto capArea = c.radius * c.radius; // * Pi
+    auto capArea = c.radius * c.radius;        // * Pi
     auto totalArea = ScalarT(2) * capArea + sideArea;
     auto part = detail::uniform01<ScalarT>(rng) * totalArea;
     if (part < sideArea) // Uniform sampling on cylinder side
@@ -249,7 +277,7 @@ template <class ScalarT, class Rng>
 {
     auto x = c.axis.pos1 - c.axis.pos0;
     auto h = length(x);
-    auto sideArea = ScalarT(2) * c.radius * h;   // * Pi, but that does not matter here
+    auto sideArea = ScalarT(2) * c.radius * h;       // * Pi, but that does not matter here
     auto capArea = ScalarT(2) * c.radius * c.radius; // * Pi
     auto totalArea = ScalarT(2) * capArea + sideArea;
     auto part = detail::uniform01<ScalarT>(rng) * totalArea;
@@ -300,7 +328,7 @@ template <class ScalarT, class Rng>
         auto l = length_sqr(p);
         if (l <= ScalarT(1))
         {
-            p *= c.base.radius;   
+            p *= c.base.radius;
             auto x = any_normal(c.base.normal);
             auto y = cross(c.base.normal, x);
             return c.base.center + p.x * x + p.y * y + (ScalarT(1) - sqrt(l)) * c.base.normal * c.height;
