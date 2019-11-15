ffcf1fc63b254faa9859b2a5c1fe78d24d1fb2fc
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2019-11-15 18:43:31 +0100

    added random_choice to feature/random

--- src/typed-geometry/feature/random.hh
+++ src/typed-geometry/feature/random.hh
@@ -2,4 +2,5 @@
 
 #include <typed-geometry/common/random.hh>
 #include <typed-geometry/functions/gaussian.hh>
+#include <typed-geometry/functions/random_choice.hh>
 #include <typed-geometry/functions/uniform.hh>
