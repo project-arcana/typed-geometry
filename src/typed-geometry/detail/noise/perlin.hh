1123e3c16938720ace19955dfbdd73c530476df8
Julian Schakib <j.schakib@outlook.com>
2020-01-09 15:35:40 +0100

    changes to run with tg, 2d perlin noise

--- src/typed-geometry/detail/noise/perlin.hh
+++ src/typed-geometry/detail/noise/perlin.hh
@@ -2,9 +2,9 @@
 
 //#include <typed-geometry/common/random.hh>
 //#include <typed-geometry/common/scalar_math.hh>
+#include <typed-geometry/detail/noise_helper.hh>
 #include <typed-geometry/types/pos.hh>
 #include <typed-geometry/types/vec.hh>
-#include <typed-geometry/detail/noise_helper.hh>
 
 namespace tg
 {
@@ -14,22 +14,19 @@ ScalarT perlin_noise(pos<2, ScalarT> P) // TODO allow overloading with ScalarT x
 {
     auto Pi = floor(pos<4, ScalarT>(P.x, P.y, P.x, P.y)) + vec<4, ScalarT>(ScalarT(0.0), ScalarT(0.0), ScalarT(1.0), ScalarT(1.0));
     auto Pf = fract(pos<4, ScalarT>(P.x, P.y, P.x, P.y)) - pos<4, ScalarT>(ScalarT(0.0), ScalarT(0.0), ScalarT(1.0), ScalarT(1.0));
-    (void)Pf;
+
     Pi = noise::mod289(Pi); // to avoid truncation effects in permutation
 
-    return 1;
-    /*auto ix = Pi.xzxz;
-    auto iy = Pi.yyww;
-    auto fx = Pf.xzxz;
-    auto fy = Pf.yyww;
+    auto ix = pos<4, ScalarT>(Pi.x, Pi.z, Pi.x, Pi.z);
+    auto iy = pos<4, ScalarT>(Pi.y, Pi.y, Pi.w, Pi.w);
+    auto fx = pos<4, ScalarT>(Pf.x, Pf.z, Pf.x, Pf.z);
+    auto fy = pos<4, ScalarT>(Pf.y, Pf.y, Pf.w, Pf.w);
 
-    auto i = permute(permute(ix) + iy);
+    auto i = noise::permute(iy + vec<4, ScalarT>(noise::permute(ix)));
 
-
-    auto gx = fract(i * (1.0 / 41.0)) * 2.0 - 1.0;
-    auto gy = abs(gx) - 0.5;
-    auto tx = floor(gx + 0.5);
-    tg::floor();
+    auto gx = fract(i * (ScalarT(1.0) / ScalarT(41.0))) * ScalarT(2.0) - ScalarT(1.0);
+    auto gy = abs(gx) - ScalarT(0.5);
+    auto tx = floor(vec<4, ScalarT>(gx) + ScalarT(0.5));
     gx = gx - tx;
 
     auto g00 = vec<2, ScalarT>(gx.x, gy.x);
@@ -37,7 +34,7 @@ ScalarT perlin_noise(pos<2, ScalarT> P) // TODO allow overloading with ScalarT x
     auto g01 = vec<2, ScalarT>(gx.z, gy.z);
     auto g11 = vec<2, ScalarT>(gx.w, gy.w);
 
-    auto norm = taylorInvSqrt(pos<4, ScalarT>(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
+    auto norm = noise::taylorInvSqrt(pos<4, ScalarT>(dot(g00, g00), dot(g01, g01), dot(g10, g10), dot(g11, g11)));
     g00 *= norm.x;
     g01 *= norm.y;
     g10 *= norm.z;
@@ -48,9 +45,9 @@ ScalarT perlin_noise(pos<2, ScalarT> P) // TODO allow overloading with ScalarT x
     auto n01 = dot(g01, vec<2, ScalarT>(fx.z, fy.z));
     auto n11 = dot(g11, vec<2, ScalarT>(fx.w, fy.w));
 
-    auto fade_xy = fade(Pf.xy);
+    auto fade_xy = noise::fade(vec<2, ScalarT>(Pf));
     auto n_x = mix(vec<2, ScalarT>(n00, n01), vec<2, ScalarT>(n10, n11), fade_xy.x);
     auto n_xy = mix(n_x.x, n_x.y, fade_xy.y);
     return ScalarT(2.3) * n_xy;
-*/}
+}
 } // namespace tg
--- src/typed-geometry/detail/noise_helper.hh
+++ src/typed-geometry/detail/noise_helper.hh
@@ -16,7 +16,12 @@ pos<4, ScalarT> mod289(pos<4, ScalarT> x)
 template <class ScalarT>
 pos<4, ScalarT> permute(pos<4, ScalarT> x)
 {
-    return mod289(((x * ScalarT(34.0)) + ScalarT(1.0)) * x);
+    // TODO * operator for pos?
+    auto ret = x;
+    auto fx = floor(x * (ScalarT(1.0) / ScalarT(289.0)));
+    for (auto i = 0; i < 4; i++)
+        ret[i] -= fx[i] * ScalarT(289.0);
+    return ret;
 }
 
 template <class ScalarT>
@@ -26,9 +31,13 @@ pos<4, ScalarT> taylorInvSqrt(pos<4, ScalarT> r)
 }
 
 template <class ScalarT>
-pos<2, ScalarT> fade(pos<2, ScalarT> t)
+pos<2, ScalarT> fade(vec<2, ScalarT> t)
 {
-    return t * t * t * (t * (t * ScalarT(6.0) - ScalarT(15.0)) + ScalarT(10.0));
+    // TODO * operator for pos?
+    auto ret = pos<2, ScalarT>::zero;
+    for (auto i = 0; i < 2; i++)
+        ret[i] = t[i] * t[i] * t[i] * (t[i] * (t[i] * ScalarT(6.0) - ScalarT(15.0)) + ScalarT(10.0));
+    return ret;
 }
 } // namespace noise
 } // namespace tg
