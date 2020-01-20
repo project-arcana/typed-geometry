1314712d8e1f1fcebc6b1440cdf242403ddca0d3
Julian Schakib <j.schakib@outlook.com>
2020-01-20 14:38:27 +0100

    1d perlin noise functions

--- src/typed-geometry/detail/noise/perlin.hh
+++ src/typed-geometry/detail/noise/perlin.hh
@@ -59,6 +59,20 @@ ScalarT perlin_noise(const ScalarT x, const ScalarT y) // TODO allow seeding, pe
 {
     return perlin_noise(pos<2, ScalarT>(x, y));
 }
+
+template <class ScalarT>
+ScalarT perlin_noise(const pos<1, ScalarT>& p) // TODO allow seeding, perlin_noise_seed()!
+{
+    // TODO is 0 possible or does that make gcc buster fail in ci?
+    return perlin_noise(pos<2, ScalarT>(p.x, ScalarT(1)));
+}
+
+template <class ScalarT>
+ScalarT perlin_noise(const ScalarT x) // TODO allow seeding, perlin_noise_seed()!
+{
+    // TODO is 0 possible or does that make gcc buster fail in ci?
+    return perlin_noise(pos<2, ScalarT>(x, ScalarT(1)));
+}
 /*
 // 3D
 template <class ScalarT>
