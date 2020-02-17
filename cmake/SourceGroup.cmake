f99b06e27c586511b085c9e0668ae3fe0d7ab6a3
Jonathan Kunstwald <jonathan.kunstwald@rwth-aachen.de>
2020-02-18 00:03:47 +0100

    Update to variadic source group macro

--- cmake/SourceGroup.cmake
+++ cmake/SourceGroup.cmake
@@ -1,9 +1,21 @@
 
 # properly groups sources for Qt Creator / Visual Studio
-macro(arcana_source_group ARG_SOURCES)
-    if (${CMAKE_GENERATOR} MATCHES "Visual Studio")
-        source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/src" FILES ${${ARG_SOURCES}})
-    else()
-        source_group("${CMAKE_CURRENT_SOURCE_DIR}/src" FILES ${${ARG_SOURCES}})
-    endif()
+#
+# usage:
+#
+# file(GLOB_RECURSE SOURCES "src/*.cc")
+# file(GLOB_RECURSE HEADERS "src/*.hh" "src/*.inl")
+# arcana_source_group(SOURCES HEADERS)
+#
+#
+macro(arcana_source_group)
+    foreach(loop_var ${ARGN})
+
+        if (${CMAKE_GENERATOR} MATCHES "Visual Studio")
+            source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}/src" FILES ${${loop_var}})
+        else()
+            source_group("${CMAKE_CURRENT_SOURCE_DIR}/src" FILES ${${loop_var}})
+        endif()
+
+    endforeach()
 endmacro()
