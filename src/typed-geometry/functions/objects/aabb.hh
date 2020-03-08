638442a37ea2a6583bf845cbd5518075d21cc8f0
Aaron Grabowy <aaron.grabowy@rwth-aachen.de>
2020-03-08 11:10:24 +0100

    Added aabb_of(box)

--- src/typed-geometry/functions/objects/aabb.hh
+++ src/typed-geometry/functions/objects/aabb.hh
@@ -3,6 +3,7 @@
 #include <typed-geometry/feature/assert.hh>
 
 #include <typed-geometry/types/objects/aabb.hh>
+#include <typed-geometry/types/objects/box.hh>
 #include <typed-geometry/types/objects/quad.hh>
 #include <typed-geometry/types/objects/segment.hh>
 #include <typed-geometry/types/objects/sphere.hh>
@@ -44,6 +45,39 @@ template <int D, class ScalarT>
 [[nodiscard]] constexpr aabb<D, ScalarT> aabb_of(quad<D, ScalarT> const& t)
 {
     return aabb_of(t.pos00, t.pos10, t.pos11, t.pos01);
+}
+
+template <class ScalarT, class TraitsT>
+[[nodiscard]] constexpr aabb<1, ScalarT> aabb_of(box<1, ScalarT, 1, TraitsT> const& b)
+{
+    return aabb_of(b[comp<1, ScalarT>(ScalarT(-1))], b[comp<1, ScalarT>(ScalarT(1))]);
+}
+template <class ScalarT, class TraitsT>
+[[nodiscard]] constexpr aabb<2, ScalarT> aabb_of(box<2, ScalarT, 2, TraitsT> const& b)
+{
+    return aabb_of(b[comp<2, ScalarT>(ScalarT(-1), ScalarT(-1))], b[comp<2, ScalarT>(ScalarT(-1), ScalarT(1))],
+                   b[comp<2, ScalarT>(ScalarT(1), ScalarT(-1))], b[comp<2, ScalarT>(ScalarT(1), ScalarT(1))]);
+}
+template <class ScalarT, class TraitsT>
+[[nodiscard]] constexpr aabb<3, ScalarT> aabb_of(box<3, ScalarT, 3, TraitsT> const& b)
+{
+    return aabb_of(b[comp<3, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(-1))], b[comp<3, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(1))],
+                   b[comp<3, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(-1))], b[comp<3, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(1))],
+                   b[comp<3, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(-1))], b[comp<3, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(1))],
+                   b[comp<3, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(-1))], b[comp<3, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(1))]);
+}
+template <class ScalarT, class TraitsT>
+[[nodiscard]] constexpr aabb<4, ScalarT> aabb_of(box<4, ScalarT, 4, TraitsT> const& b)
+{
+    return aabb_of(
+        b[comp<4, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(-1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(-1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(-1), ScalarT(-1), ScalarT(1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(-1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(-1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(-1), ScalarT(1), ScalarT(1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(-1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(-1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(1), ScalarT(-1), ScalarT(1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(-1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(-1), ScalarT(1))],
+        b[comp<4, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(1), ScalarT(-1))], b[comp<4, ScalarT>(ScalarT(1), ScalarT(1), ScalarT(1), ScalarT(1))]);
 }
 
 template <class PrimA, class PrimB, class... PrimsT>
