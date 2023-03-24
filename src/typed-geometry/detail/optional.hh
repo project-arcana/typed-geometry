3310da4bcb030989fc833defeabd7d62de3657e8
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2023-03-24 10:30:53 +0100

    Improve warning message

--- src/typed-geometry/detail/optional.hh
+++ src/typed-geometry/detail/optional.hh
@@ -1,7 +1,7 @@
 #pragma once
 
 // Add this message because the [[deprecated]] attribute for tg::optional does not create warnings because it is templated
-#pragma message("Header <typed-geometry/detail/optional.hh> is deprecated")
+#pragma message("Header <typed-geometry/detail/optional.hh> is deprecated. Please use cc::optional / #include <clean-core/optional.hh> instead.")
 
 #include <clean-core/optional.hh>
 
