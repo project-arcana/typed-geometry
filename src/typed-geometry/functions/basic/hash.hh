6c06dcc4b4870de21370444e658f7ad6b45da67f
Julius Nehring-Wirxel <julius.nehring-wirxel@rwth-aachen.de>
2023-11-23 15:37:46 +0100

    move tg::hash into clean-core namespace to ease usage with cc containers

--- src/typed-geometry/functions/basic/hash.hh
+++ src/typed-geometry/functions/basic/hash.hh
@@ -2,8 +2,19 @@
 
 #include <typed-geometry/tg-lean.hh>
 
+#include <clean-core/hash.hh>
+
 namespace tg
 {
+template <class T>
+using hash = cc::hash<T>;
+
+template <class T>
+constexpr size_t make_hash(T const& v) noexcept
+{
+    return hash<T>{}(v);
+}
+
 namespace detail
 {
 struct hash_inspector
@@ -11,9 +22,10 @@ struct hash_inspector
     size_t hash = 0x987654321;
 
     template <class T, class... Args>
-    constexpr void operator()(T const& v, Args&&...)
+    constexpr size_t operator()(T const& v, Args&&...)
     {
         add(v);
+        return hash;
     }
 
 private:
@@ -40,9 +52,8 @@ private:
         introspect(*this, const_cast<T&>(v)); // v will not be changed
     }
 };
-}
 
-// universal hasher for tg types
+//// universal hasher for tg types
 struct hash
 {
     template <class T>
@@ -53,10 +64,231 @@ struct hash
         return hi.hash;
     }
 };
+}
+}
+
+// extend cc::hash for tg types
+namespace cc
+{
+
+// ------------------------------------ objects ------------------------------------ //
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::aabb<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int ObjectD, class ScalarT, int DomainD, class TraitsT>
+struct hash<tg::box<ObjectD, ScalarT, DomainD, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::capsule<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::cylinder<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int ObjectD, class ScalarT, int DomainD, class TraitsT>
+struct hash<tg::ellipse<ObjectD, ScalarT, DomainD, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::frustum<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::halfspace<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::hemisphere<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::inf_cone<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::inf_cylinder<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT, class TraitsT>
+struct hash<tg::inf_frustum<D, ScalarT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::line<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::plane<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <class BaseT, class TraitsT>
+struct hash<tg::pyramid<BaseT, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::quad<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::ray<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::segment<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int ObjectD, class ScalarT, int DomainD, class TraitsT>
+struct hash<tg::sphere<ObjectD, ScalarT, DomainD, TraitsT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::triangle<D, ScalarT>> : tg::detail::hash
+{
+};
+
+// ------------------------------------ scalars ------------------------------------ //
+
+template <int words>
+struct hash<tg::fixed_int<words>> : tg::detail::hash
+{
+};
+
+template <int words>
+struct hash<tg::fixed_uint<words>> : tg::detail::hash
+{
+};
 
 template <class T>
-constexpr size_t make_hash(T const& v) noexcept
+struct hash<tg::fwd_diff<T>> : tg::detail::hash
 {
-    return hash{}(v);
-}
+};
+
+// todo: half
+
+template <class T>
+struct hash<tg::interval<T>> : tg::detail::hash
+{
+};
+
+// todo: quater
+
+// ------------------------------------ comp-likes ------------------------------------ //
+
+template <class T>
+struct hash<tg::angle_t<T>> : tg::detail::hash
+{
+};
+
+template <class T, uint64_t N>
+struct hash<tg::array<T, N>> : tg::detail::hash
+{
+};
+
+template <int Degree, class ControlPointT>
+struct hash<tg::bezier<Degree, ControlPointT>> : tg::detail::hash
+{
+};
+
+template <class T, size_t N>
+struct hash<tg::capped_array<T, N>> : tg::detail::hash
+{
+};
+
+template <class T, size_t N>
+struct hash<tg::capped_vector<T, N>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::color<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::comp<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::dir<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <class ScalarT>
+struct hash<tg::hsl_t<ScalarT>> : tg::detail::hash
+{
+};
+
+template <class ScalarT>
+struct hash<tg::hsla_t<ScalarT>> : tg::detail::hash
+{
+};
+
+template <int C, int R, class ScalarT>
+struct hash<tg::mat<C, R, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::pos<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::quadric<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <class ScalarT>
+struct hash<tg::quaternion<ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D>
+struct hash<tg::range<D>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::size<D, ScalarT>> : tg::detail::hash
+{
+};
+
+template <class ScalarT>
+struct hash<tg::srgb<ScalarT>> : tg::detail::hash
+{
+};
+
+template <class ScalarT>
+struct hash<tg::srgba<ScalarT>> : tg::detail::hash
+{
+};
+
+template <int D, class ScalarT>
+struct hash<tg::vec<D, ScalarT>> : tg::detail::hash
+{
+};
+
 }
