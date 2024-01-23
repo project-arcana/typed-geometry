5f292274bb0a8b9d481d5731f82daa28457d6724
Philip Trettner <trettner@shapedcode.com>
2024-01-23 14:26:47 +0100

    made triangulation_of(aabb3) supersolid

--- src/typed-geometry/functions/objects/triangulation.hh
+++ src/typed-geometry/functions/objects/triangulation.hh
@@ -51,33 +51,34 @@ template <class ScalarT, class TraitsT>
     return r;
 }
 
-// normal_of(t) points outwards
+/// normal_of(t) points outwards
+/// guarantees that the result has 8 unique vertices (for non-empty aabbs)
 template <class ScalarT, class TraitsT>
 [[nodiscard]] constexpr array<triangle<3, ScalarT>, 12> triangulation_of(aabb<3, ScalarT, TraitsT> const& b)
 {
-    int idx = 0;
     array<triangle<3, ScalarT>, 12> r;
 
-    auto dir = b.max - b.min;
+    auto const v000 = pos<3, ScalarT>(b.min.x, b.min.y, b.min.z);
+    auto const v001 = pos<3, ScalarT>(b.min.x, b.min.y, b.max.z);
+    auto const v010 = pos<3, ScalarT>(b.min.x, b.max.y, b.min.z);
+    auto const v011 = pos<3, ScalarT>(b.min.x, b.max.y, b.max.z);
+    auto const v100 = pos<3, ScalarT>(b.max.x, b.min.y, b.min.z);
+    auto const v101 = pos<3, ScalarT>(b.max.x, b.min.y, b.max.z);
+    auto const v110 = pos<3, ScalarT>(b.max.x, b.max.y, b.min.z);
+    auto const v111 = pos<3, ScalarT>(b.max.x, b.max.y, b.max.z);
 
-    auto add_face = [&](pos<3, ScalarT> const& p, vec<3, ScalarT> const& d0, vec<3, ScalarT> const& d1) {
-        auto const p00 = p;
-        auto const p10 = p + d0;
-        auto const p01 = p + d1;
-        auto const p11 = p + d0 + d1;
-
-        // TODO: orientation guarantees?
-        r[idx++] = {p00, p01, p11};
-        r[idx++] = {p00, p11, p10};
-    };
-
-    add_face(b.min, {dir.x, 0, 0}, {0, dir.y, 0});
-    add_face(b.min, {0, dir.y, 0}, {0, 0, dir.z});
-    add_face(b.min, {0, 0, dir.z}, {dir.x, 0, 0});
-
-    add_face(b.max, {0, -dir.y, 0}, {-dir.x, 0, 0});
-    add_face(b.max, {0, 0, -dir.z}, {0, -dir.y, 0});
-    add_face(b.max, {-dir.x, 0, 0}, {0, 0, -dir.z});
+    r[0] = {v000, v010, v110};
+    r[1] = {v000, v110, v100};
+    r[2] = {v000, v001, v011};
+    r[3] = {v000, v011, v010};
+    r[4] = {v000, v100, v101};
+    r[5] = {v000, v101, v001};
+    r[6] = {v111, v001, v101};
+    r[7] = {v111, v011, v001};
+    r[8] = {v111, v100, v110};
+    r[9] = {v111, v101, v100};
+    r[10] = {v111, v010, v011};
+    r[11] = {v111, v110, v010};
 
     return r;
 }
