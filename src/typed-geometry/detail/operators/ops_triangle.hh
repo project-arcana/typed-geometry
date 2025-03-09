141180e2e573cc2c5905d027214ea68de605f0f6
Philip Trettner <trettner@shapedcode.com>
2025-03-09 14:26:14 +0100

    added tri+pos

--- src/typed-geometry/detail/operators/ops_triangle.hh
+++ src/typed-geometry/detail/operators/ops_triangle.hh
@@ -29,6 +29,26 @@ template <int D, class ScalarT>
 }
 
 template <int D, class ScalarT>
+[[nodiscard]] constexpr triangle<D, ScalarT> operator+(triangle<D, ScalarT> const& a, pos<D, ScalarT> const& b)
+{
+    triangle<D, ScalarT> r;
+    r.pos0 = a.pos0 + b;
+    r.pos1 = a.pos1 + b;
+    r.pos2 = a.pos2 + b;
+    return r;
+}
+
+template <int D, class ScalarT>
+[[nodiscard]] constexpr triangle<D, ScalarT> operator-(triangle<D, ScalarT> const& a, pos<D, ScalarT> const& b)
+{
+    triangle<D, ScalarT> r;
+    r.pos0 = a.pos0 - b;
+    r.pos1 = a.pos1 - b;
+    r.pos2 = a.pos2 - b;
+    return r;
+}
+
+template <int D, class ScalarT>
 [[nodiscard]] constexpr triangle<D, ScalarT> operator*(triangle<D, ScalarT> const& a, ScalarT b)
 {
     triangle<D, ScalarT> r;
